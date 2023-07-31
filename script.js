da32c1e3dca753c57863d49de6a05065c128069d
FlorianHass <florianhass22@gmail.com>
2023-07-31 23:15:38 +0200

    Commit Number 1

---
+++ script.js
@@ -0,0 +1,35 @@
+
+/*Object date with methods*/
+const date = new Date()
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+const day_in_month = [31,28,31,30,31,30,31,31,30,31,30,31]
+
+/* Render based on a constant variable in backend */
+/*calenderbodyrender(){
+    
+}
+
+const rendermonth = month () {
+
+}*/
+
+/* Look at jquery and jquery user interface */
+
+
+
+
+function selectOptionCreateYear() {
+
+    select = document.getElementById('year');
+    select.add(new Option(10))
+    /*var year = new Date().getFullYear();
+    line = "";
+    for (var i = -20; i < 21 ; i++) {   
+      opt = document.createElement('option');
+      opt.value = i+year;
+      opt.innerHTML = i+year;
+      select.appendChild(line);
+    }*/
+}
