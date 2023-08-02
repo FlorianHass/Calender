573916a15d83411afe13aed9c02ded4d862b2998
FlorianHass <florianhass22@gmail.com>
2023-08-02 23:10:01 +0200

    Boah man Tag fuckt ab

--- README.md
+++ README.md
@@ -1,12 +1,36 @@
 # Calender
 Calender for Webengineering Course
 
-Functions:
+1. `selectOptionCreateMonth(selectElement, stringArray, datearray)`: This function populates the month selection dropdown with options from the `months` array. If a selected month is stored in the session, it sets the dropdown's value accordingly.
 
-reloadsaves() => Function that works whenever calender dates change, saves are made, reload using AJAX
-loaddate() => At beginning of session loads current date, gets used to generate calender, does not work when refreshed
-jumpdate() => Use form to change relying on day, month, year
+2. Event listener for `selectmonth`: When the month selection is changed, it triggers the `reloadDaysFromMonth` function to update the days in the day selection dropdown based on the selected month.
 
+3. `reloadDaysFromMonth(selectedMonth)`: This function populates the day selection dropdown based on the number of days in the selected month.
 
+4. Event listener for `selectday`: Saves the selected day value in the session when the day selection is changed.
 
-The variable 'date' is reloaded first at the beginning of the session and whenever the user chooses a new datr -> two functions
+5. `selectOptionCreateYear()`: This function populates the year selection dropdown with options for the past 50 years and the next 50 years. If a selected year is stored in the session, it sets the dropdown's value accordingly.
+
+6. Event listener for `selectyear`: Saves the selected year value in the session when the year selection is changed.
+
+7. `returndate()`: This function returns the selected date from the session if available; otherwise, it returns the current date.
+
+8. `handleSubmit(event)`: This function handles the form submission and updates the global `date` variable based on the selected year, month, and day. It also reloads the page after form submission.
+
+9. Event listener for the form submission (`dateForm`): Triggers the `handleSubmit` function when the form is submitted.
+
+10. `loadview()`: This function is responsible for displaying the appropriate view based on the `view` stored in the session.
+
+11. `getDaysInMonth(year, month)`: Returns the number of days in a given month (accounting for leap years).
+
+12. `createCalendarMonth(year, month)`: Generates a calendar table for the selected month, displaying the days of the month in a grid format.
+
+13. `createCalendarWeek(year, month, day)`: Generates a calendar table for a week starting from the provided `year`, `month`, and `day`. It displays the dates of the week in a single row.
+
+14. `createCalendarDay(year, month, day)`: Generates a calendar table for a day based on the provided `year`, `month`, and `day`. It displays only the selected date.
+
+15. `setmonthview()`, `setweekview()`, `setdayview()`: These functions are used to set the `view` in the session and then call `loadview()` to display the appropriate view.
+
+16. `loadview()`: Initially loads the appropriate view based on the `view` stored in the session or defaults to the month view.
+
+The script uses these functions to create a dynamic date selection calendar that allows the user to choose a date from a year, month, and day dropdown. The selected date is displayed in a table with different views based on the selected option (`month`, `week`, or `day`).
--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-/*Object date with methods*/
-const date = new Date()
 const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const monthsvalue =["01","02","03","04","05","06","07","08","09","10","11","12"];
@@ -8,6 +6,7 @@ const monthsvalue =["01","02","03","04","05","06","07","08","09","10","11","12"]
 const selectyear = document.getElementById('YearDateSelect');
 const selectmonth = document.getElementById('MonthDateSelect');
 const selectday = document.getElementById('DayDateSelect');
+const Lengthofday = 8.64e+7;
 
 
 function selectOptionCreateMonth(selectElement, stringArray, datearray) {
@@ -22,8 +21,11 @@ function selectOptionCreateMonth(selectElement, stringArray, datearray) {
     selectElement.appendChild(option);
   };
 
-  if (localStorage.getItem('selectedMonth')) {
-    selectElement.value = localStorage.getItem('selectedMonth');
+  if (sessionStorage.getItem('selectedmonth')) {
+    selectElement.value = sessionStorage.getItem('selectedmonth');
+  }
+  else{
+    selectElement.value = datearray[new Date().getMonth()];
   }
 }
 
@@ -37,13 +39,19 @@ selectmonth.addEventListener('change', (event) => {
 
 /* Save the selected month locally for session*/
 selectmonth.addEventListener('change', function() {
-  localStorage.setItem('selectedMonth', selectmonth.value);
+  sessionStorage.setItem('selectedmonth', selectmonth.value);
 });
 
-
-
-// Initial load of the Day options based on the default selected month (January)
-reloadDaysFromMonth(monthsvalue[0]);
+// Load of the Day options based on the month when page is first loaded
+window.onload = function () {
+  if (sessionStorage.getItem("hasCodeRunBefore") === null) {
+      reloadDaysFromMonth(monthsvalue[new Date().getMonth()]);
+      sessionStorage.setItem("hasCodeRunBefore", true);
+  }
+  else{
+    reloadDaysFromMonth(sessionStorage.getItem('selectedmonth'));
+  }
+}
 
 function reloadDaysFromMonth(selectedMonth) {
   const daySelectElement = document.getElementById('DayDateSelect');
@@ -67,7 +75,7 @@ function reloadDaysFromMonth(selectedMonth) {
 
 /* Save the selected day locally for session*/
 selectday.addEventListener('change', function() {
-  localStorage.setItem('selectedDay', selectday.value);
+  sessionStorage.setItem('selectedday', selectday.value);
 });
 
 
@@ -95,9 +103,9 @@ function selectOptionCreateYear() {
     selectElement.appendChild(yearOption);
   }
 
-  // Set the index of the current year as selected or if in the same session someone already choose an option that remains from localstorage
-  if (localStorage.getItem('selectedYear')) {
-    selectElement.value = localStorage.getItem('selectedYear');
+  // Set the index of the current year as selected or if in the same session someone already choose an option that remains from sessionStorage
+  if (sessionStorage.getItem('selectedyear')) {
+    selectElement.value = sessionStorage.getItem('selectedyear');
   }
   else{
     selectElement.selectedIndex = 50;
@@ -109,5 +117,271 @@ selectOptionCreateYear();
 
 /* Save the selected year locally for session*/
 selectyear.addEventListener('change', function() {
-  localStorage.setItem('selectedYear', selectyear.value);
+  sessionStorage.setItem('selectedyear', selectyear.value);
 });
+
+/*Set the global date*/
+const date = returndate();
+
+function returndate() {
+  if (sessionStorage.getItem('date')) {
+    return sessionStorage.getItem('date');
+  }
+  else{
+    return new Date();
+  }
+}
+
+// Function to get the selected date from the form and set the global 'date' variable
+function handleSubmit(event) {
+  event.preventDefault(); // Prevent form submission
+
+  const selectedYear = selectyear.value;
+  const selectedMonth = selectmonth.value;
+  const selectedDay = selectday.value;
+
+  // Set the global 'date' variable to the selected date
+  const currentdate = `${selectedYear}-${selectedMonth}-${selectedDay}`;
+  console.log("Selected Date:", currentdate);
+  sessionStorage.setItem('date',currentdate);
+  sessionStorage.setItem('selectedyear', selectedYear);
+  sessionStorage.setItem('selectedmonth', selectedMonth);
+  sessionStorage.setItem('selectedday', selectedDay);
+  /*Refresh the page*/
+  location.reload();
+}
+
+// Event listener to handle form submission
+const dateForm = document.getElementById('dateForm');
+dateForm.addEventListener('submit', handleSubmit);
+
+function loadview(){
+  const calendarDiv = document.getElementById('calendartable');
+  calendarDiv.innerHTML = "";
+  switch (sessionStorage.getItem('view')) {
+    case "month": /*Month*/
+      const calendarTable = createCalendarMonth(sessionStorage.getItem("selectedyear"), sessionStorage.getItem("selectedmonth"));
+      calendarDiv.appendChild(calendarTable);
+      break;
+    
+    case "week": /*Week*/
+      const calendarTable2 = createCalendarWeek(sessionStorage.getItem("selectedyear"), sessionStorage.getItem("selectedmonth"),sessionStorage.getItem("selectedday"));
+      calendarDiv.appendChild(calendarTable2);
+      break;
+
+    case "day": /*Day*/
+    const calendarTable3 = createCalendarDay(sessionStorage.getItem("selectedyear"), sessionStorage.getItem("selectedmonth"),sessionStorage.getItem("selectedday"));
+    calendarDiv.appendChild(calendarTable3);
+      break;
+    
+    default:
+      const calendarTable4 = createCalendarMonth(sessionStorage.getItem("selectedyear"), sessionStorage.getItem("selectedmonth"));
+      calendarDiv.appendChild(calendarTable4);
+      break;
+  }
+}
+
+function getDaysInMonth(year, month) {
+  // Return the number of days in a given month (month is 0-indexed)
+  return new Date(year, month, 0).getDate();
+}
+
+function createCalendarMonth(year, month) {
+  // Create a new Date object with the given year and month (month is 0-indexed)
+  const firstDayOfMonth = new Date(year+"-"+month+"-"+"01");
+
+  // Get the first day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+  const firstDayOfWeek = firstDayOfMonth.getDay();
+
+  // Create the calendar table
+  const table = document.createElement('calendertable');
+  table.style.borderCollapse = 'collapse';
+
+  // Create the table header with day names
+  const headerRow = document.createElement('tr');
+  const daysOfWeek = ['Sun','Mon','Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+  for (let i = 0; i < 7; i++) {
+      const headerCell = document.createElement('th');
+      headerCell.textContent = daysOfWeek[i];
+      headerCell.style.border = '1px solid black';
+      headerCell.style.padding = '5px';
+      headerRow.appendChild(headerCell);
+  }
+
+  table.appendChild(headerRow);
+
+  // Create the table rows for the days in the month
+  const daysInMonth = getDaysInMonth(year, month);
+  let currentDate = 1;
+
+  for (let row = 0; row < 6; row++) {
+      const tableRow = document.createElement('tr');
+
+      for (let col = 0; col < 7; col++) {
+          const tableCell = document.createElement('td');
+
+          if (row === 0 && col < firstDayOfWeek) {
+              // Empty cell before the first day of the month
+              tableCell.textContent = '';
+          } else if (currentDate > daysInMonth) {
+              // Empty cell after the last day of the month
+              tableCell.textContent = '';
+          } else {
+              // Display the current date
+              tableCell.textContent = currentDate;
+              currentDate++;
+          }
+
+          tableCell.style.border = '1px solid black';
+          tableCell.style.padding = '5px';
+          tableRow.appendChild(tableCell);
+      }
+
+      table.appendChild(tableRow);
+  }
+
+  return table;
+}
+
+function createCalendarWeek(year, month, day) {
+  // Create the calendar table
+  const table = document.createElement('calendertable');
+  table.style.borderCollapse = 'collapse';
+
+  // Create the table header with day names
+  const headerRow = document.createElement('tr');
+  const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+  for (let i = 0; i < 7; i++) {
+    const headerCell = document.createElement('th');
+    headerCell.textContent = daysOfWeek[i];
+    headerCell.style.border = '1px solid black';
+    headerCell.style.padding = '5px';
+    headerRow.appendChild(headerCell);
+  }
+  table.appendChild(headerRow);
+
+  // Calculate the start and end dates for the week
+  const startDate = new Date(year+"-"+month+"-"+day);
+  const firstdateofweek = new Date(startDate.valueOf(startDate) - startDate.getDay()* Lengthofday);
+  
+  // Populate the table cells with the dates for the week
+  const tableRow = document.createElement('tr');
+
+  for (let col = 0; col < 7; col++) {
+    const tableCell = document.createElement('td');
+    const dateValue = new Date(firstdateofweek.valueOf(firstdateofweek) + col * Lengthofday).getDate();
+    tableCell.textContent = dateValue;
+
+    tableCell.style.border = '1px solid black';
+    tableCell.style.padding = '5px';
+    tableRow.appendChild(tableCell);
+  }
+
+  table.appendChild(tableRow);
+  return table;
+}
+
+function createCalendarDay(year,month,day) {
+  // Create the calendar table
+  const table = document.createElement('calendertable');
+  table.style.borderCollapse = 'collapse';
+  const currentdate = new Date(year+"-"+month+"-"+day);
+  const headerRow = document.createElement('tr');
+  const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+  const headerCell = document.createElement('th');
+  headerCell.textContent = daysOfWeek[currentdate.getDay()];
+  headerCell.style.border = '1px solid black';
+  headerCell.style.padding = '5px';
+  headerRow.appendChild(headerCell);
+  table.appendChild(headerRow);
+
+  // Populate the table cells with the dates for the week
+  const tableRow = document.createElement('tr');
+
+    const tableCell = document.createElement('td');
+    tableCell.textContent = currentdate.getDate();
+
+    tableCell.style.border = '1px solid black';
+    tableCell.style.padding = '5px';
+    tableRow.appendChild(tableCell);
+
+  table.appendChild(tableRow);
+
+  return table;
+}
+
+
+function setmonthview(){
+  sessionStorage.setItem("view","month");
+  loadview();
+}
+
+function setweekview(){
+  sessionStorage.setItem("view","week");
+  loadview();
+}
+
+function setdayview(){
+  sessionStorage.setItem("view","day");
+  loadview();
+}
+
+loadview();
+
+function goforthinview(){
+  switch (sessionStorage.getItem("view")) {
+    case "month":
+      
+      const countmonth = parseInt(sessionStorage.getItem("selectedmonth"));
+      if (countmonth === 12) {
+        countyear = parseInt(sessionStorage.getItem("selectedyear"));
+        sessionStorage.setItem("selectedyear", (countyear+1).toString() );
+        sessionStorage.setItem("selectedmonth",monthsvalue[0]);
+      }
+      else if (countmonth < 9){
+        sessionStorage.setItem("selectedmonth","0"+(countmonth+1).toString());
+      }
+      else {
+        sessionStorage.setItem("selectedmonth",(countmonth+1).toString());
+      }
+      break;
+  
+    case "week":
+      const currentdate = new Date(sessionStorage.getItem("selectedyear")+"-"+sessionStorage.getItem("selectedmonth")+"-"+sessionStorage.getItem("selectedday"));
+      const nextdate = new Date(currentdate.valueOf(currentdate) + 7*Lengthofday);
+
+      sessionStorage.setItem("selectedyear",nextdate.getFullYear());
+      sessionStorage.setItem("selectedmonth",monthsvalue[nextdate.getMonth()])
+      if (parseInt(nextdate.getDate) < 10) {
+        sessionStorage.setItem("selectday","0"+nextdate.getDate());
+      }
+      else{
+      sessionStorage.setItem("selectday",nextdate.getDate());
+      }
+      break;
+
+    case "day":
+      const currentdate2 = new Date(sessionStorage.getItem("selectedyear")+"-"+sessionStorage.getItem("selectedmonth")+"-"+sessionStorage.getItem("selectedday"));
+      const nextdate2 = new Date(currentdate2.valueOf(currentdate2) + Lengthofday);
+
+      sessionStorage.setItem("selectedyear",nextdate2.getFullYear());
+      sessionStorage.setItem("selectedmonth",monthsvalue[nextdate2.getMonth()])
+      if (parseInt(nextdate2.getDate) < 10) {
+        sessionStorage.setItem("selectday","0"+nextdate2.getDate());
+      }
+      else{
+      sessionStorage.setItem("selectday",nextdate2.getDate());
+      }
+      break;
+
+    default:
+
+      break;
+  }
+  selectOptionCreateMonth(selectmonth,months,monthsvalue);
+  selectOptionCreateYear();
+  loadview();
+}
